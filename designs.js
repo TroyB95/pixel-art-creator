94d20e438ee2877076df8d9a1aca5f7afb969041
TroyB <troy_budd95@msn.com>
2018-03-14 20:46:01 +0000

    Added event listeners to the Color and size inputs. Created the function to produce the grid when submit is pressed.

--- designs.js
+++ designs.js
@@ -1,10 +1,53 @@
 // Select color input
+const colorPicker = document.querySelector('#colorPicker');
+let color = '';
+
+colorPicker.addEventListener('change', function(e){
+	 color = e.target.value;
+	 console.log(color);
+})
+
 // Select size input
+let width = 0;
+let height = 0;
+
+const sizePicker = document.querySelector('#sizePicker');
+const heightSelector = document.querySelector('#inputHeight');
+const widthSelector = document.querySelector('#inputWidth');
+
+widthSelector.addEventListener('change', function(e){
+	width = e.target.value;
+})
+
+heightSelector.addEventListener('change', function(e){
+	height = e.target.value;
+})
+
+sizePicker.addEventListener('submit', function(e){
+	e.preventDefault();
+	makeGrid();
+	
+})
 
 // When size is submitted by the user, call makeGrid()
+const canvas = document.querySelector('#pixelCanvas');
+
+
+
 
 function makeGrid() {
+	canvas.innerHTML = '';
 
-// Your code goes here!
+	for(var i = 0; i <= height; i++){
+		const row = document.createElement('TR');
+		canvas.appendChild(row);
+
+		for(var j = 0; j <= width; j++){
+			const col = document.createElement('TD');
+			row.appendChild(col);
+			
+		}
+	}
+	console.log('hello');
 
 }
